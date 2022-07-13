0aac4167ee2b4e0b86e316b299e929101da85b07
rudollf <rudolfmukhammadiev1@gmail.com>
2022-07-13 16:04:24 +0500

    Navbar has been added

--- src/components/talants/talants.js
+++ src/components/talants/talants.js
@@ -1,29 +1,63 @@
 import React from 'react';
-import {Link} from "react-router-dom";
 import RoutesPath from "../../routes/routes";
+import {Link, useNavigate} from "react-router-dom";
+import {Button} from "react-bootstrap";
 import {connect} from "react-redux";
 
 function Talants(props) {
+    const navigate = useNavigate();
     return (
         <div>
             <div className="post-jop-wrapper" id="overflow-y-scroll">
-                <div className="container">
-                    <div className="d-flex napa_logo justify-content-between align-items-center">
-                        <Link to="/">
-                            <img className="napa" src="/images/logo_napa.svg" alt="Error"/>
-                        </Link>
-                        <div className="menu">
-                            <ul className="nav h-100">
-                                <li className="nav-item"><Link className="nav-link" to={RoutesPath.home}>Home</Link></li>
-                                <li className="nav-item"><Link className="nav-link" to="/">Talants</Link></li>
-                                <li className="nav-item"><Link className="nav-link" to={RoutesPath.postJop}>Post job</Link>
+            <div className={"navbarHom"}>
+                <div className="logoBg"><img src="./images/Napa_logo_white.svg" alt=""/></div>
+                <div className="homeNavbar">
+                    <div className=" navbar navbar-expand-md">
+                        <button type="button" className="navbar-toggler knopka" data-bs-toggle="collapse"
+                                data-bs-target="#myCollapse">
+                            <label htmlFor="check">
+                                <input type="checkbox" id="check"/>
+                                <span></span>
+                                <span></span>
+                            </label>
+                        </button>
+
+                        <div className="collapse navbar-collapse" id="myCollapse">
+
+                            <ul className="nav">
+                                <li className="nav-item">
+                                    <div className='nav-link' data-bs-toggle="tab" onClick={() =>{
+                                        navigate(RoutesPath.home)}} >Home</div>
                                 </li>
-                                <li className="nav-item"><Link className="nav-link" to={RoutesPath.aboutUs}>About us</Link>
+                                <li className="nav-item">
+                                    <div className="nav-link active" data-bs-toggle="tab"
+                                         onClick={() =>{navigate(RoutesPath.talants)}}>Talants</div>
                                 </li>
-                                <li className="nav-item"><Link className="nav-link" to="/">Contact us</Link></li>
+                                <li className="nav-item">
+                                    <div className="nav-link" data-bs-toggle="tab"
+                                         onClick={() =>{navigate(RoutesPath.postJop)}}>Jobs</div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="nav-link" data-bs-toggle="tab"
+                                         onClick={() =>{navigate(RoutesPath.aboutUs)}}>About us</div>
+                                </li>
+                                <li className="nav-item">
+                                    <div className="nav-link" data-bs-toggle="tab">Contact us</div>
+                                </li>
                             </ul>
+                            <div className='boxBtn'>
+                                <Button className="custom-outline-btn" onClick={() =>{
+                                    navigate(RoutesPath.login)}}>Log in</Button>
+                                <Button className="custom-outline-btn sign-btn" onClick={() =>{
+                                    navigate(RoutesPath.signUp)}}>Sign up</Button>
+                            </div>
                         </div>
                     </div>
+                </div>
+            </div>
+                <div className="container">
+                <div>
+        </div>
                     <div className="row">
                         <div className="col-9">
                             <div className="input-group bg-white py-3 px-3 mb-3 w-100" style={{borderRadius: "6px"}}>
